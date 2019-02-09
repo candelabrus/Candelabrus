225c2c815c568db46a6c94793110add4091457b0
Cláudio Pereira <git@claudiop.com>
2019-02-09 11:47:55 +0000

    Serve media based on DEBUG flag.

--- source/urls.py
+++ source/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from candelabrus import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('markdownx/', include('markdownx.urls')),
     path('admin/', admin.site.urls),
     path('fo/', include('fons.urls')),
@@ -12,3 +14,6 @@ urlpatterns = [
     path('nz/', include('nietz.urls')),
     path('mk/', include('mereokratos.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
