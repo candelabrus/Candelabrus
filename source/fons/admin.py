a7ed94b40510aa0d5411ce6ef740e9cda539ee04
Cláudio Pereira <git@claudiop.com>
2019-02-20 01:00:03 +0000

    Fons: Model admin.

---
+++ source/fons/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from fons import models as m
+from django_mptt_admin.admin import DjangoMpttAdmin
+
+
+class LocalizedSubjectAdmin(admin.TabularInline):
+    model = m.LocalizedSubject
+
+
+class SubjectAdmin(DjangoMpttAdmin):
+    inlines = [
+        LocalizedSubjectAdmin,
+    ]
+
+
+class LocalizedEventAdmin(admin.TabularInline):
+    model = m.LocalizedEvent
+
+
+class EventDivisionAdmin(admin.TabularInline):
+    model = m.EventDivisions
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        LocalizedEventAdmin,
+        EventDivisionAdmin
+    ]
+
+
+admin.site.register(m.Subject, SubjectAdmin)
+admin.site.register(m.Event, EventAdmin)
+admin.site.register(m.SourceProvider)
+admin.site.register(m.Source)
+admin.site.register(m.Webpage)
+admin.site.register(m.Media)
+admin.site.register(m.SourceFlaw)
+admin.site.register(m.Citation)
